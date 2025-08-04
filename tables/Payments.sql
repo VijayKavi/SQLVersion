e0802d94fd7b75ddd825afa6497e02c038925da6
VijayKavi <vijayakumarthangavel@histogenetics.com>
2025-08-04 23:30:35 +0530

    tables

---
+++ tables/Payments.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Payments](
+	[PaymentID] [int] NOT NULL,
+	[OrderID] [int] NULL,
+	[PaymentDate] [date] NULL,
+	[Amount] [decimal](10, 2) NULL,
+	[PaymentMethod] [nvarchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[PaymentID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
---
+++ tables/Table_1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Table_1](
+	[name] [nchar](10) NULL,
+	[age] [nchar](10) NULL,
+	[address] [nchar](10) NULL
+) ON [PRIMARY]
